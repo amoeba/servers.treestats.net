11455307106635555a80d73cbd973df7bd3756e0
Bryce Mecum <petridish@gmail.com>
2021-02-01 23:53:58 -0900

    Tweak payload

--- app.rb
+++ app.rb
@@ -8,6 +8,7 @@ heartbeats = []
 
 get '/' do
   return "no heartbeats yet" if heartbeats.length <= 0
+
   @list = heartbeats.sort_by{ |b| b[:updated_at]}.reverse!
 
   erb :index
@@ -17,15 +18,13 @@ post '/' do
   json = JSON.parse(request.body.read)
 
   heartbeat = {
-    :server => json["server"],
-    :online => json["online"],
-    :xp_modifier => json["xp_modifier"],
-    :pk_server => json["pk_server"],
+    :WorldName => json["server"],
+    :OnlineCount => json["online"],
     :updated_at => Time.now.utc.iso8601
   }
 
   heartbeats.push(heartbeat);
 
   status 200
-  return { "response": "success"}.to_json
+  return ""
 end
